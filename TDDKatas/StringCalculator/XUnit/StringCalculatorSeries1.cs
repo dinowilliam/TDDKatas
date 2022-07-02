e8167556bd32e08e9d981319267a9e57b67fc084
William Ferreira de Paula <739447+dinowilliam@users.noreply.github.com>
2022-07-01 22:47:24 -0300

    Clean Up Code

--- TDDKatas/StringCalculator/XUnit/StringCalculatorSeries1.cs
+++ TDDKatas/StringCalculator/XUnit/StringCalculatorSeries1.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TDDKatas.StringCalculator.XUnit {
+﻿namespace TDDKatas.StringCalculator.XUnit {
     public class StringCalculatorSeries1 {
         
         public object Add(string numbers) {
