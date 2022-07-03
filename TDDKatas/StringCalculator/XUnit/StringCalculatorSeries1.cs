0adc0f601edd45a9db5414d14e216ae384abbe53
William Ferreira de Paula <739447+dinowilliam@users.noreply.github.com>
2022-07-03 15:25:46 -0300

    Refactor to eliminate loop in the split using a select parse to int

--- TDDKatas/StringCalculator/XUnit/StringCalculatorSeries1.cs
+++ TDDKatas/StringCalculator/XUnit/StringCalculatorSeries1.cs
@@ -2,19 +2,10 @@
     public class StringCalculatorSeries1 {
         
         public object Add(string numbers) {
-            Int32 value = 0;
-
-            var splitNumbers = numbers.Split(",", StringSplitOptions.RemoveEmptyEntries);
-
-            if (splitNumbers.Any() && splitNumbers.Length > 0) {
-
-                foreach (var number in splitNumbers) {                     
-                        value += Convert.ToInt32(number);
-                }
-
-            }
-
-            return value;
+            
+            var splitNumbers = numbers.Split(",", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse);
+                       
+            return splitNumbers.Sum();                        
         }
     }
 }
